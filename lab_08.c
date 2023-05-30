f8ef1b7b214e2b6359df4c5425f135e8c56d92bf
Asdergh <1132222465@pfur.ru>
2023-05-30 13:31:46 +0300

    all changes were

--- lab_08.c
+++ lab_08.c
@@ -1,7 +1,29 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include <string.h>
+
 
 int main(){
-    printf("hello world");
+    FILE *fp;
+    fp = fopen("text.txt", "r");
+    char string[100];
+    char tmp_string[100];
+    if ( fgets(string, 100, fp) == NULL){
+        printf("empty file");
+    }
+    else{
+        for(int iter = 0; iter < sizeof(string); iter++){
+            if(&string[iter] == ";"){
+                printf("%s", tmp_string);
+                for(int iter_tmp = 0; iter_tmp < sizeof(tmp_string); iter_tmp++){
+                    tmp_string[iter_tmp] = "\0";
+                }
+            }
+            else{
+                tmp_string[iter] = &string[iter];
+                printf("%c", tmp_string[iter]);
+            }
+        }
+    }
     
 }
